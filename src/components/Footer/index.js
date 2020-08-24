f39b8439117a1444ee3819db4624b98986589bea
Hank Miles <Hankshadow2000@yahoo.com>
2020-08-24 15:01:14 -0400

    uploaded to github

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Inventory Tracker 2020</span>
+    </footer>
+  );
+}
+
+export default Footer;
